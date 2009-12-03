7136c776f543a017ec296f3a2b81796501aed1e3
Steve Jenson <stevej@twitter.com>
2009-12-03 11:50:01 -0800

    deleting my homemade haskell-mode init.el, moving into modes.

--- customizations/modes.el
+++ customizations/modes.el
@@ -1,2 +1,9 @@
 ;; textmate-minor-mode
 (textmate-mode)
+
+;; haskell-mode
+(load "haskell-site-file")
+(add-hook 'haskell-mode-hook 'turn-on-haskell-doc-mode)
+(add-hook 'haskell-mode-hook 'turn-on-haskell-indent)
+;; (add-hook 'haskell-mode-hook 'turn-on-haskell-simple-indent)
+(add-hook 'haskell-mode-hook 'turn-on-haskell-ghci)
