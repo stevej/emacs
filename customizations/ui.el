1f24c112d431fa2d40531bcbb50243d870b47801
Steve Jenson <stevej@twitter.com>
2009-12-03 12:27:16 -0800

    killed my .emacs dead, suckers!

--- customizations/ui.el
+++ customizations/ui.el
@@ -10,3 +10,17 @@
 (setq default-frame-alist
       '((font . "-apple-inconsolata-medium-r-normal--20-140-72-72-m-140-iso10646-1")))
 
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+'(blink-cursor-mode t)
+'(column-number-mode t)
+'(show-paren-mode t))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+)
