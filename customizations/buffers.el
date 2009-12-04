49a3f84c1bb81534f3ea3e4d12809182a93aa448
Steve Jenson <stevej@twitter.com>
2009-12-04 11:40:49 -0800

    let's handle buffer management separately.

---
+++ customizations/buffers.el
@@ -0,0 +1,10 @@
+; save minibuffer history across sessions
+(setq savehist-file "~/.emacs.d/.savehist")
+(savehist-mode 1)
+
+; nicer naming of buffers with identical names
+(require 'uniquify)
+(setq uniquify-buffer-name-style 'reverse)
+(setq uniquify-separator " • ")
+(setq uniquify-after-kill-buffer-p t)
+(setq uniquify-ignore-buffers-re "^\\*")
