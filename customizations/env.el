e62d207b90bafc2b025ed8a1af7ec30bc4a72a04
Steve Jenson <stevej@twitter.com>
2009-12-07 14:59:08 -0800

    exec-path for shelling out.

--- customizations/env.el
+++ customizations/env.el
@@ -1,2 +1,5 @@
 (setq path "/bin:/usr/local/bin:/usr/local/mysql/bin:/usr/bin:/usr/sbin:/Users/stevej/bin")
-(setenv "PATH" path))
+(setenv "PATH" path)
+
+(push "/opt/local/bin" exec-path)
+(push "/usr/local/bin" exec-path)
