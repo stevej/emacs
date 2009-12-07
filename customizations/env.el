d4d5dc735b9da064679b183504025f8ddaf7a4c8
Steve Jenson <stevej@twitter.com>
2009-12-07 15:04:38 -0800

    more PATH!

--- customizations/env.el
+++ customizations/env.el
@@ -1,4 +1,4 @@
-(setq path "/bin:/usr/local/bin:/usr/local/mysql/bin:/usr/bin:/usr/sbin:/Users/stevej/bin")
+(setq path "/bin:/usr/local/bin:/usr/local/mysql/bin:/usr/bin:/usr/sbin:/Users/stevej/bin:/opt/local/bin")
 (setenv "PATH" path)
 
 (push "/opt/local/bin" exec-path)
