4e2226d7c41ed309b2e1756889b6a20e0b989902
Steve Jenson <stevej@twitter.com>
2009-12-16 15:13:37 -0800

    colors for magit mode and remove unused magit dir

--- customizations/ui.el
+++ customizations/ui.el
@@ -17,9 +17,24 @@
 '(blink-cursor-mode t)
 '(column-number-mode t)
 '(show-paren-mode t))
+
+(setq font-lock-global-modes '(not magit-mode w3m-mode))
+
+; pretty diff-mode
 (custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
-)
+ '(diff-added ((t (:foreground "#559944"))))
+ '(diff-context ((t nil)))
+ '(diff-file-header ((((class color) (min-colors 88) (background dark)) (:foreground "RoyalBlue1"))))
+ '(diff-function ((t (:foreground "#00bbdd"))))
+ '(diff-header ((((class color) (min-colors 88) (background dark)) (:foreground "RoyalBlue1"))))
+ '(diff-hunk-header ((t (:foreground "#fbde2d"))))
+ '(diff-nonexistent ((t (:inherit diff-file-header :strike-through nil))))
+ '(diff-refine-change ((((class color) (min-colors 88) (background dark)) (:background "#182042"))))
+ '(diff-removed ((t (:foreground "#de1923")))))
+
+; pretty magit diffs (based on colors for diff-mode above)
+(set-face-attribute 'magit-diff-add nil :foreground "#008838" :background "#ddffdd")
+(set-face-attribute 'magit-diff-del nil :foreground "#c0002b" :background "#ffeeee")
+(set-face-attribute 'magit-diff-file-header nil :foreground "RoyalBlue1")
+(set-face-attribute 'magit-diff-hunk-header nil :foreground "RoyalBlue1")
+(set-face-attribute 'magit-item-highlight nil :background "#eeeeee")
