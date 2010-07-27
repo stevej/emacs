739fa54453a276065ef0fff27cb3a5077c61ab14
Steve Jenson <stevej@twitter.com>
2010-07-27 10:31:04 -0700

    using ELPA again

--- init.el
+++ init.el
@@ -84,3 +84,14 @@
     (load
      (expand-file-name "~/.emacs.d/elpa/package.el"))
   (package-initialize))
+
+
+;;; This was installed by package-install.el.
+;;; This provides support for the package system and
+;;; interfacing with ELPA, the package archive.
+;;; Move this code earlier if you want to reference
+;;; packages in your .emacs.
+(when
+    (load
+     (expand-file-name "~/.emacs.d/elpa/package.el"))
+  (package-initialize))
