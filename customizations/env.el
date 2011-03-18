edef8231a8eb76eadc11f71bdd2922904df3deae
Steve Jenson <stevej@twitter.com>
2011-03-18 09:38:19 -0700

    adding ensime to our PATH

--- customizations/env.el
+++ customizations/env.el
@@ -1,2 +1,2 @@
-(setq path "/bin:/usr/local/bin:/usr/local/mysql/bin:/usr/bin:/usr/sbin:/Users/stevej/bin:/opt/local/bin")
+(setq path "/bin:/usr/local/bin:/usr/local/mysql/bin:/usr/bin:/usr/sbin:/Users/stevej/bin:/opt/local/bin:/Users/stevej/local/ensime")
 (setenv "PATH" path)
