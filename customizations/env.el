dccf5d66dd617335a7cfd3041eb0bea1b00bf283
Steve Jenson <stevej@fruitless.org>
2011-03-26 11:25:41 -0700

    adding scala to my path and exec-path

--- customizations/env.el
+++ customizations/env.el
@@ -1,2 +1,5 @@
-(setq path "/bin:/usr/local/bin:/usr/local/mysql/bin:/usr/bin:/usr/sbin:/Users/stevej/bin:/opt/local/bin:/Users/stevej/local/ensime")
+(setq path "/bin:/usr/local/bin:/usr/local/mysql/bin:/usr/bin:/usr/sbin:/Users/stevej/bin:/opt/local/bin:/Users/stevej/local/ensime:/Users/stevej/local/scala-2.8.1.final/bin")
 (setenv "PATH" path)
+
+
+(setq exec-path (append exec-path (split-string path ":")))
